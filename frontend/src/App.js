a2d2b784143476a5963b1263f2a13ffd871c8ae0
unknown <orhan.zcan@gmail.com>
2020-09-12 18:13:20 +0300

    düzeltme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ export default function () {
     <Router>
       <ToastProvider>
         <AuthProvider>
-          {/* <NavigationProvider> */}
+          {/* <NavigationProviderxasas> */}
           <App />
           {/* </NavigationProvider> */}
         </AuthProvider>
