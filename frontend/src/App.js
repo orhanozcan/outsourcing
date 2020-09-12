c1ad83ebd886804b240a3f5cf1708796c9797805
omer faruk <omer.faruk.aydin@promis.com.tr>
2020-09-12 18:34:36 +0300

    dedede e

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ export default function () {
     <Router>
       <ToastProvider>
         <AuthProvider>
-          {/* <NavigationProvis sds > */}
+          {/* <NavigationProvis sdsasdasdas > */}
           <App />
           {/* </NavigationProvider> */}
         </AuthProvider>
